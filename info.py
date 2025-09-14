1277633905e684b3c5d6d8d226fa35561b0a21c0
molly4252 <mollydeanx1@gmail.com>
2025-09-14 08:30:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'Deendayal_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '25471015'))
+API_HASH = environ.get('API_HASH', '0ab2955ad5b1a913e220800b5fc5db36')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7722696426:AAGmuSYoC8o7EbVcSRdgX6ODMEWTxhf5FNA")
 # ============================
 # Bot Settings Configuration
 # ============================
